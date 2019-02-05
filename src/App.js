fddeeeae89a45f50d314633a0ecdd1293669f457
Pablo Adriaensens Albiñana <pablo.adriaensens@gmail.com>
2019-02-05 12:43:39 +0100

    Saving the news in the state

--- src/App.js
+++ src/App.js
@@ -2,11 +2,34 @@ import React, {Component} from 'react';
 import Header from "./componentes/Header";
 
 class App extends Component {
+
+    state = {
+        noticias: {}
+    };
+
+    componentDidMount() {
+        this.consultarNoticias();
+    }
+
+    consultarNoticias = () => {
+        let url = 'https://newsapi.org/v2/top-headlines?sources=cnn-es&apiKey=cedad511f5ba4da4b074c3ce57f58456';
+
+        fetch(url)
+            .then(respuesta => {
+                return respuesta.json();
+            })
+            .then(noticias => {
+                this.setState({
+                    noticias: noticias.articles
+                })
+            })
+    };
+
     render() {
         return (
             <div className="App">
                 <Header
-                    titulo = 'Noticas'
+                    titulo='Noticas'
                 />
             </div>
         );
